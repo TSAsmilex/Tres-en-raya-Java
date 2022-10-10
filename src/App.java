78e9d53b52caedcb959b7184ca972d953736f1a2
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-10 15:43:26 +0200

    Terminado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        clearScreen();
+        System.out.println("¡Bienvenido!");
+        Thread.sleep(1200);
+
+        var firstPlayerStarts = (new Random()).nextBoolean();
+        var player1 = firstPlayerStarts? Options.X : Options.O;
+        var player2 = firstPlayerStarts? Options.O : Options.X;
+
+
+        var players = new Options[] {player1, player2};
+        var current = 0;
+
+        System.out.println("Empieza el jugador " + players[current]);
+        Thread.sleep(2000);
+        clearScreen();
+
+        var game = new Board();
+        Scanner scan = new Scanner(System.in);
+
+        while (!game.finished()) {
+            clearScreen();
+            System.out.println(game.toString());
+            System.out.println("Mueve ficha, jugador " + players[current].toString());
+
+            int i = -1, j = -1;
+
+            do {
+                i = scan.nextInt();
+                j = scan.nextInt();
+            } while (!game.put(i-1, j-1, players[current]));
+
+            current = (current + 1) % 2;
+        }
+
+        scan.close();
+
+        clearScreen();
+        System.out.println(game.toString());
+        System.out.println("¡Enhorabuena! Has ganado, jugador " + players[current].toString() + ".") ;
+        Thread.sleep(100);
+        System.out.println("Eres un máquina fiera mastodonte.");
+        Thread.sleep(500);
+    }
+
+
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
---
+++ src/Board.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+
+enum Options {
+    X ("X"),
+    O ("O"),
+    BLANK ("_");
+
+    private final String name;
+
+    private Options(String s) {
+        name = s;
+    }
+
+    public boolean equalsName(String otherName) {
+        // (otherName == null) check is not needed because name.equals(null) returns false
+        return name.equals(otherName);
+    }
+
+    public String toString() {
+       return this.name;
+    }
+
+}
+
+public class Board {
+    ArrayList<ArrayList<Options>> board;
+    int size = 3;
+
+    public Board() {
+        board = new ArrayList<ArrayList<Options>>();
+
+        for (int i = 0; i < size; i++) {
+            board.add(new ArrayList<Options>());
+            for (int j = 0; j < size; j++) {
+                board.get(i).add(Options.BLANK);
+            }
+        }
+    }
+
+    public boolean put(int row, int col, Options option) {
+        if (row < 0 || row > size) {
+            return false;
+        }
+        else if (col < 0 || col > size) {
+            return false;
+        }
+        else if (board.get(row).get(col) == Options.BLANK) {
+            board.get(row).set(col, option);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public boolean finished() {
+        // Check horizontals
+        for (int i = 0; i < size; i++) {
+            if (    board.get(i).get(0) == board.get(i).get(1) && board.get(i).get(1) == board.get(i).get(2)
+                &&  board.get(i).get(0) != Options.BLANK) {
+                return true;
+            }
+        }
+
+        // Check verticals
+        for (int i = 0; i < size; i++) {
+            if (    board.get(0).get(i) == board.get(1).get(i) && board.get(1).get(i) == board.get(2).get(i)
+                &&  board.get(0).get(i) != Options.BLANK) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (    board.get(0).get(0) == board.get(1).get(1) && board.get(1).get(1) == board.get(2).get(2)
+            &&  board.get(0).get(0) != Options.BLANK) {
+            return true;
+        }
+        else if (   board.get(0).get(2) == board.get(1).get(1) && board.get(1).get(1) == board.get(2).get(0)
+                 && board.get(0).get(2) != Options.BLANK) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        String result = "  1 2 3\n";
+
+        for (int i = 0; i < board.size(); i++) {
+            var line = board.get(i);
+            result += i+1 + " ";
+
+            for (var item: line) {
+
+                switch (item) {
+                    case X -> result += "X";
+                    case O -> result += "O";
+                    case BLANK -> result += "_";
+                }
+                result += " ";
+            }
+            result += "\n";
+        }
+
+        return result;
+    }
+}
